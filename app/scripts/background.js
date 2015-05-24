edba250adad5ac50d04b0e2c736df65fd4ee99cc
Taras Romaniv <t.romaniv@gmail.com>
2015-05-24 18:37:03 +0300

    Updated manifest, minor changes

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,11 +1,3 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
-
-chrome.tabs.onUpdated.addListener(function (tabId) {
-  chrome.pageAction.show(tabId);
-});
-
-console.log('\'Allo \'Allo! Event Page for Page Action');
+console.log('Event Page for Page Action');
